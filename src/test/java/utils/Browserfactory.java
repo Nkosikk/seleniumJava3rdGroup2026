45008ed437145d22b58efa012f08e2bb4c27801a
Ennie Maneke <enniemaneke@gmail.com>
2026-06-16 01:14:51 +0200

    Created BrowserFactory

---
+++ src/test/java/utils/Browserfactory.java
@@ -0,0 +1,38 @@
+package utils;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class Browserfactory {
+
+  public static WebDriver driver;
+
+    public static void StartBrowser(String browserType, String url){
+
+        if (browserType.equalsIgnoreCase( "ChRome" ));{
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments( "--incognito" );
+            driver = new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase( "FireFox" ));{
+            driver = new FirefoxDriver();
+
+        } else if(browserType.equalsIgnoreCase( "Safari" ));{
+             driver = new SafariDriver();
+
+        } else {
+            driver = new EdgeDriver();
+        }
+
+        driver.get(url);
+        driver.manage().window().maximize();
+
+        return driver;
+
+    }
+
+}
