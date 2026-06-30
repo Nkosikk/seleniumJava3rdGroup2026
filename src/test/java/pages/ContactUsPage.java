4893fddbf1e5b763b4611884fb1beab35616fcf6
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-06-30 19:26:17 +0200

    executed screenshots class

--- src/test/java/pages/ContactUsPage.java
+++ src/test/java/pages/ContactUsPage.java
@@ -3,7 +3,10 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,6 +22,7 @@ public class ContactUsPage {
     }
 
     public void clickContactUsButton(){
+        new WebDriverWait(driver, Duration.ofSeconds(60)).until(ExpectedConditions.elementToBeClickable(contactUs_xpath));
         driver.findElement(contactUs_xpath).click();
     }
 
--- src/test/java/tests/NdosiTests.java
+++ src/test/java/tests/NdosiTests.java
@@ -3,23 +3,28 @@ package tests;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 import utils.Base;
+import utils.TakeScreenshots;
 
 public class NdosiTests extends Base {
 
 
     @Test
-    public void loginTest()  {
+    public void loginTest() throws InterruptedException {
         loginPage.clickLoginMainButton();
         loginPage.enterUsername("nkwanyana@gmail.com");
         loginPage.enterPassword("#12345678");
         loginPage.clickLoginButton();
         landingPage.welcomeText();
+        Thread.sleep(30);
+        TakeScreenshots.takeSnapshot(driver, "LoginScreen");
     }
 
     @Test
-    public void contactUsTest() {
+    public void contactUsTest() throws InterruptedException {
         contactUsPage.clickContactUsButton();
         contactUsPage.contactMethodsText();
+        Thread.sleep(60);
+        TakeScreenshots.takeSnapshot(driver, "contactUsScreen");
     }
 
     @AfterClass
